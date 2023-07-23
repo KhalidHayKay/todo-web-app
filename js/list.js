da073f8e95c3eafdb912ce1c72bca1c6dc8ec0e0
Alamutu Khalid <alamutukhalid@gmail.com>
2023-07-23 04:27:32 +0100

    todolist path: sessionstorage --> screen

--- js/list.js
+++ js/list.js
@@ -1,8 +1,10 @@
+//      Show textarea
 const addTaskBtn = document.querySelector('div.add-task');
 const addTaskTextarea = document.querySelector('div.task-textarea');
 
 addTaskBtn.addEventListener('click', showTextArea);
-function showTextArea(){
+function showTextArea()
+{
     addTaskTextarea.style.display = 'flex';
 }
 
@@ -10,7 +12,8 @@ function showTextArea(){
 const listContainer = document.querySelector('section.todo-list ul');
 const empty = document.querySelector('section.todo-list ul .empty');
 
-if(listContainer.childElementCount < 2){
+if(listContainer.childElementCount < 2)
+{
     empty.style.display = 'block';
 }
 
@@ -35,29 +38,25 @@ function textareaBtnsEvent(e){
     addTaskTextarea.style.display = 'none';
 }
 
-/*        List (Viewing and Editing)
-
-const todoLists = document.querySelectorAll('section.todo-list ul li .detail');
-
-todoLists.forEach(todoList => {
-    todoList.addEventListener('click', e => {
-        const todoListText = Array.from(todoList.children)[0];
-        textarea.value = todoListText.textContent;
-        showTextArea();
+//      Todo lists saved in SessionStorage
+let todoListArr;
+if(sessionStorage.getItem('todolist-array') === null)
+{
+    todoListArr = [];
+} else
+{
+    todoListArr = JSON.parse(sessionStorage.getItem('todolist-array'));
+    todoListArr.forEach(task => {
+        printTask(task.task, task.date);
     })
-}) */
-
-
+}
 
 
 //      FUNCTIONS!!!!!
 
-let todoListArr = [];
 
-function addNewTask(){
-    const taskName = textarea.value;
-    const currentDate = DateAndTime();
-
+function printTask(taskName, currentDate)
+{
     const newTask = document.createElement('li');
     newTask.innerHTML = `
         <div class="detail">
@@ -70,30 +69,31 @@ function addNewTask(){
         </div>
     `;
     empty.style.display = 'none';
-    todoListArr.push(newTask);
-
-    console.log(newTask);
     listContainer.appendChild(newTask);
-    
-    /* List (Viewing and Editing)
 
+    // List (Viewing and *Editing)
     newTask.firstElementChild.addEventListener('click', e => {
         const todoListText = newTask.firstElementChild.firstElementChild;
         textarea.value = todoListText.textContent;
         showTextArea();
 
-        textareaBtns[0].style.visibility = 'hidden';
-        textareaBtns.forEach(btn => {
-            btn.removeEventListener('click', textareaBtnsEvent);
-        })
-        textareaBtns[1].addEventListener('click', e => {
-            todoListText.textContent = textarea.value;
+        // textarea.readOnly = true;
+        // textareaBtns[1].style.visibility = 'hidden';
+    })
+}
 
-            textarea.value = '';
-            addTaskTextarea.style.display = 'none';
-        })
 
-    }) */
+function addNewTask()
+{
+    newTaskProps = {
+        task: textarea.value,
+        date: DateAndTime()
+    }
+
+    todoListArr.push(newTaskProps);
+    sessionStorage.setItem('todolist-array', JSON.stringify(todoListArr))
+
+    printTask(newTaskProps.task, newTaskProps.date);
 }
 
 
