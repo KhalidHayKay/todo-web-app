5d36925c0e7c98deaf40dafc8a12e62269c4e4bd
Alamutu Khalid <alamutukhalid@gmail.com>
2023-08-02 14:11:04 +0100

    Archive section improved

--- js/archive.js
+++ js/archive.js
@@ -1,33 +1,65 @@
-import { storage } from './list.js';
+import { storage, Task, DateAndTime, todoListArr } from './list.js';
 
 const archivedTaskContainer = document.querySelector('.archives ul');
 let archivedTaskArr = storage.get('archiveArr') === null ? [] : storage.get('archiveArr');
+
+window.addEventListener('DOMContentLoaded', e => {
+    archivedTaskArr.forEach(list => {
+        const archive = new TaskArchive(list.name, list.taskId)
+        archive.printArchive();
+    });
+})
 
 class TaskArchive
 {
     constructor(name, id)
     {
         this.name = name;
-        this.id = id;
+        this.taskId = id;
+    }
+
+    #buildElement()
+    {
+        const element = document.createElement('li');
+        element.innerHTML =  `
+            <p>${this.name}</p>
+            <i class='bx bx-archive-out' data-id=${this.taskId}></i>
+        `
+        return element;
+    }
+
+    printArchive()
+    {
+        archivedTaskContainer.appendChild(this.#buildElement());
+        // Unarchive events
+        archivedTaskContainer.addEventListener('click', e => {
+            e.stopImmediatePropagation();
+
+            if(e.target.classList.contains('bx-archive-out')){
+                const newTask = new Task(
+                    e.target.previousElementSibling.textContent,
+                    DateAndTime(),
+                    e.target.dataset.id
+                )
+                newTask.print();
+                todoListArr.push(newTask);
+                storage.set('listArr', todoListArr);
+
+                e.target.parentElement.remove();
+                archivedTaskArr = storage.delete(archivedTaskArr, e.target.dataset.id);
+                storage.set('archiveArr', archivedTaskArr);
+            }
+        })
+    }
+
+    autoRemoveArchive()
+    {
+        setTimeout(() => {
+            archivedTaskContainer.removeChild();
+        }, 10000)
     }
 }
 
-function printArchive()
-{
-    const archiveElementArr = archivedTaskArr.map(list => {
-        return `
-            <li>
-                <p>${list.name}</p>
-                <i class='bx bx-archive-out' data-id=${list.id}></i>
-            </li>
-        `
-    })
 
-    archivedTaskContainer.innerHTML = archiveElementArr.join('');
 
-    setTimeout( () => {
-        archivedTaskContainer.innerHTML = '';
-    }, 10000)
-}
-
-export { archivedTaskArr, TaskArchive, printArchive };
+export { archivedTaskArr, TaskArchive};
--- js/list.js
+++ js/list.js
@@ -1,4 +1,4 @@
-import { archivedTaskArr, TaskArchive, printArchive } from "./archive.js";
+import { archivedTaskArr, TaskArchive } from "./archive.js";
 
 class Task
 {
@@ -8,87 +8,29 @@ class Task
         this.date = date;
         this.taskId = taskId;
     }
-}
 
-// Print previous stored tasks
-window.addEventListener('DOMContentLoaded', e => {
-    task.print();
-    printArchive();
-})
-
-// Get HTML elements
-const addTaskBtn = document.querySelector('div.add-task');
-const addTaskTextarea = document.querySelector('div.task-textarea');
-const textarea = document.querySelector('div.task-textarea>textarea');
-const listContainer = document.querySelector('section.todo-list ul');
-
-addTaskBtn.addEventListener('click', e => {
-    textArea.show();
-
-    addTaskTextarea.addEventListener('click', e => {
-        e.stopImmediatePropagation();
-        textArea.textareaBtnsEvent(e);
-    })
-});
-
-
-
-//Textarea
-const textArea = {
-    textareaBtnsEvent: function(e)
+    #buildElement()
     {
-        if(e.target.textContent === 'save'){
-            if(textarea.value === ''){
-                alert('Task cannot be empty');
-            } else {
-                const randomId = Math.random() * 1000000;
-                let newTask = new Task(
-                    textarea.value,
-                    task.DateAndTime(),
-                    randomId
-                );
-                todoListArr.push(newTask);
-                storage.set('listArr', todoListArr);
-        
-                task.print();
-
-                textarea.value = '';
-                textArea.hide();
-            }
-        } else if(e.target.textContent === 'cancel'){
-            textArea.hide();
-        }
-    },
-
-    show: function(){
-        addTaskTextarea.style.display = 'flex';
-    },
-    hide: function(){
-        addTaskTextarea.style.display = 'none';
+        const element = document.createElement('li');
+        element.innerHTML =  `
+            <div class="detail">
+                <p>${this.taskName}</p>
+                <span>${this.date}</span>
+            </div>
+            <div class="icons">
+                <i class='bx bxs-pencil data-id=${this.taskId}'></i>
+                <i class='bx bxs-trash-alt' data-id=${this.taskId}></i>
+            </div>
+        `
+        return element
     }
-}
 
-//Task
-const task = {
-    print: function()
+    print()
     {
-        
-        const elementArr = todoListArr.map(list => {
-            return `
-                <li>
-                    <div class="detail">
-                        <p>${list.taskName}</p>
-                        <span>${list.date}</span>
-                    </div>
-                    <div class="icons">
-                        <i class='bx bxs-pencil'></i>
-                        <i class='bx bxs-trash-alt' data-id=${list.taskId}></i>
-                    </div>
-                </li>
-            `
-        }) 
-        listContainer.innerHTML = elementArr.join('');
-        // delete task
+        listContainer.appendChild(this.#buildElement());
+        checkTodolistEmptiness();
+
+        // list events
         listContainer.addEventListener('click', e => {
             if(e.target.classList.contains('bxs-trash-alt')){
                 task.delete(e);
@@ -100,14 +42,106 @@ const task = {
                 task.edit(e);
             }
         })
+    }
+}
+
+//Storage
+const storage = {
+    set: (key, arr) => {
+        sessionStorage.setItem(key, JSON.stringify(arr));
+    },
+    get: (key) => {
+        return JSON.parse(sessionStorage.getItem(key));
+    },
+    delete: (arr, id) => {
+        return arr.filter(list => list.taskId != id);
+    },
+}
+
+function checkTodolistEmptiness()
+{
+    const empty = document.querySelector('section.todo-list ul .empty');
+    if(listContainer.lastElementChild === empty)
+    { 
+        empty.style.display = 'block';
+    } else{
+        empty.style.display = 'none';
+    }
+}
+
+// Main todolist array
+let todoListArr = storage.get('listArr') === null ? [] : storage.get('listArr');
+// Get HTML elements
+const addTaskBtn = document.querySelector('div.add-task');
+const addTaskTextarea = document.querySelector('div.task-textarea');
+const textarea = document.querySelector('div.task-textarea>textarea');
+const listContainer = document.querySelector('section.todo-list ul');
+
+// Print previous stored tasks
+window.addEventListener('DOMContentLoaded', e => {
+    todoListArr.forEach(task => {
+        const newTask = new Task(task.taskName, task.date, task.taskId);
+        newTask.print();
+    })
+
+    checkTodolistEmptiness();
+})
+
+// Textarea opener
+addTaskBtn.addEventListener('click', e => {
+    textArea.show();
+
+    addTaskTextarea.addEventListener('click', e => {
+        e.stopImmediatePropagation();
+        textArea.textareaBtnEvents(e);
+    })
+});
+
+
+const textArea = {
+    show: function(){
+        addTaskTextarea.style.display = 'flex';
+    },
+    hide: function(){
+        addTaskTextarea.style.display = 'none';
     },
 
+    textareaBtnEvents: function(e)
+    {
+        if(e.target.textContent === 'save'){
+            if(textarea.value === ''){
+                alert('Task cannot be empty');
+            } else {
+                const randomId = Math.random() * 1000000;
+                let newTask = new Task(
+                    textarea.value,
+                    DateAndTime(),
+                    randomId
+                );
+                newTask.print();
+
+                todoListArr.push(newTask);
+                storage.set('listArr', todoListArr);
+
+                textarea.value = '';
+                textArea.hide();
+            }
+        } else if(e.target.textContent === 'cancel'){
+            textArea.hide();
+        }
+    },
+}
+
+//Task
+const task = {
     delete: function(e)
     {
         e.target.parentElement.parentElement.remove();
 
-        todoListArr = todoListArr.filter(list => list.taskId != e.target.dataset.id);
+        todoListArr = storage.delete(todoListArr, e.target.dataset.id);
         storage.set('listArr', todoListArr);
+
+        checkTodolistEmptiness();
     },
 
     // Edit (and viewing)
@@ -130,61 +164,34 @@ const task = {
         })
     },
 
-    //archive
     archive: function(e)
     {
+        e.stopImmediatePropagation();
         const name = e.target.parentElement.previousElementSibling.firstElementChild.textContent;
         const id = e.target.nextElementSibling.dataset.id;
         const newTaskArchive = new TaskArchive(name, id);
+        newTaskArchive.printArchive();
+
         archivedTaskArr.push(newTaskArchive);
         storage.set('archiveArr', archivedTaskArr);
 
-        // const archivedTaskContainer = document.querySelector('.archives ul');
-        printArchive();
+        todoListArr = storage.delete(todoListArr, id)
+        this.delete(e);
     },
-
-    DateAndTime: function()
-    {
-        const dateFunction = new Date();
-
-        const date = dateFunction.getDate();
-        const month = dateFunction.toLocaleString('default', {month: 'short'});
-        const year = dateFunction.getFullYear();
-        const hour = dateFunction.getHours().toString().padStart(2, 0);
-        const minute = dateFunction.getMinutes().toString().padStart(2, 0);
-
-        return `${hour}:${minute} ${month} ${date}, ${year}.`;
-    }
 }
 
-// UI (task list)
-// const ui = {
-//     checkTodolistEmptiness: function()
-//     {
-//         const empty = document.querySelector('section.todo-list ul .empty');
-//         console.log(empty)
-//         if(listContainer.childElementCount < 6)
-//         {
-//             // empty.style.display = 'block';
-//             listContainer.innerHTML = empty;
-//         }
-//         console.log(listContainer.childElementCount)
-//         console.log('a')
-//     },
-// }
-// ui.checkTodolistEmptiness();
+function DateAndTime()
+{
+    const dateFunction = new Date();
 
-//Storage
-const storage = {
-    set: function(key, arr){
-        sessionStorage.setItem(key, JSON.stringify(arr));
-    },
-    get: function(key){
-        return JSON.parse(sessionStorage.getItem(key));
-    }
+    const date = dateFunction.getDate();
+    const month = dateFunction.toLocaleString('default', {month: 'short'});
+    const year = dateFunction.getFullYear();
+    const hour = dateFunction.getHours().toString().padStart(2, 0);
+    const minute = dateFunction.getMinutes().toString().padStart(2, 0);
+
+    return `${hour}:${minute} ${month} ${date}, ${year}.`;
 }
 
-let todoListArr = storage.get('listArr') === null ? [] : storage.get('listArr');
 
-
-export { storage };
+export { storage, Task, DateAndTime, todoListArr };
